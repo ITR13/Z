b9c28cfa04879aa9a397996ec2e9c73916573b6d
ITR <ITR@live.no>
2018-10-28 02:01:04 +0100

    Z

---
+++ Controller.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public static float Lenght = 20;
+
+    private float _invincible = 0f;
+
+    private Transform _lifeT, _timeT;
+    private float _life, _time;
+
+    private Renderer _renderer;
+    private Rigidbody2D _rigidBody;
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<Renderer>();
+        _invincible = 3;
+
+        _bed = GameObject.Find("Bed");
+        _office = GameObject.Find("Office");
+
+        _lifeT = transform.Find("Life");
+        _timeT = transform.Find("Time");
+        _lifeT.GetComponent<Renderer>().material.color = Color.blue;
+        _timeT.GetComponent<Renderer>().material.color = Color.green;
+        _life = 1;
+        _time = 1;
+    }
+
+    Vector2 _direction;
+    private void Update()
+    {
+        _direction = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical")
+        );
+        _invincible -= Time.deltaTime;
+        if (_invincible >= 0)
+        {
+            _renderer.material.color = Color.red;
+        }
+        else
+        {
+            _renderer.material.color = Color.white;
+        }
+        _life -= Time.deltaTime / (4 * Lenght);
+        _time -= Time.deltaTime / (Lenght);
+
+        _lifeT.localScale = new Vector3(_life, 0.05f, 1);
+        _timeT.localScale = new Vector3(_time, 0.05f, 1);
+
+        if (_life < 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (_time < 0)
+        {
+            Switch();
+        }
+    }
+
+    private GameObject _bed, _office;
+    private bool _day = true;
+    private void Switch()
+    {
+        _bed.SetActive(_day);
+        _day = !_day;
+        _office.SetActive(_day);
+        _invincible = _day ? 3 : 0;
+        _time += 1;
+        if (_day && Lenght > 6)
+        {
+            Lenght -= 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = _direction * 5;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Z"))
+        {
+            collision.transform.localPosition *= -1;
+            if (_day)
+            {
+                _life -= 0.2f;
+            }
+            else
+            {
+                _life += 0.2f;
+            }
+        }
+    }
+}
